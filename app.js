8b110fa83efa0726540c526cc828e2cfa0d7eb60
alyar88 <lsd00000>
2018-11-23 17:46:33 +0300

    init

--- app.js
+++ app.js
@@ -27,7 +27,11 @@ app.set('view engine', 'hbs');
 // 	methods: ['POST'],
 // }
 
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 // app.use(function(req, res, next) {
 //
 // 	const origin = req.headers.origin;
