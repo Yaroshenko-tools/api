07d0cdfe350ae64a68865c236052633104a27b5d
alyar88 <lsd00000>
2018-12-14 17:35:18 +0300

    ad generator

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ app.set('view engine', 'hbs');
 // }
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.header('Access-Control-Allow-Credentials', true);
 	next();
 });
 // app.use(function(req, res, next) {
